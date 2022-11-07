3186ae196e653c5f9dcc7c4cb94706cd3c9b2caa
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2022-11-07 18:27:55 +0900

    first commit

---
+++ src/components/greet/Greet.test.tsx
@@ -0,0 +1,19 @@
+/**
+ * Greet
+ * hello 문자가 보이고 name props 가 주어지면 함께 보여진다
+ */
+
+import { render, screen } from '@testing-library/react';
+import Greet from './greet';
+
+test('Greet render correctly', () => {
+  render(<Greet />);
+  const textEl = screen.getByText(/hello/i);
+  expect(textEl).toBeInTheDocument();
+});
+
+test('Greet renders with a name', () => {
+  render(<Greet name="Hyeok" />);
+  const textEl = screen.getByText(/hello hyeok/i);
+  expect(textEl).toBeInTheDocument();
+});
